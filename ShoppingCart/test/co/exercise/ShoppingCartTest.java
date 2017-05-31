565407af8cc2f4a94408015b983a337faf224d43
Aslı ATAKAN <Asl ATAKAN@HP840G3Win10.intra.local>
2017-05-31 15:16:20 +0100

    initial

---
+++ ShoppingCart/test/co/exercise/ShoppingCartTest.java
@@ -0,0 +1,14 @@
+package co.exercise;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShoppingCartTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
